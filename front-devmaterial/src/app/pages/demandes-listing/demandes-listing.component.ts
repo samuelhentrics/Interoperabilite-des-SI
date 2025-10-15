ab334fb07fd62d30f9d18c484aca2017302d72e1
Samuel Hentrics <samuel.hentrics@gmail.com>
2025-10-15 17:47:38 +0200

    FRONT

---
+++ front-devmaterial/src/app/pages/demandes-listing/demandes-listing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demandes-listing',
+  imports: [],
+  templateUrl: './demandes-listing.component.html',
+  styleUrl: './demandes-listing.component.scss'
+})
+export class DemandesListingComponent {
+
+}
