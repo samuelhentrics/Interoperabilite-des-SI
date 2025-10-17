64f333655b88e655ab8d3b7afe79906a0f34b296
Samuel Hentrics <samuel.hentrics@gmail.com>
2025-10-17 08:39:44 +0200

    demande edit in progress + xml test for front

--- front-wagonlits/src/app/app.routes.ts
+++ front-wagonlits/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { DemandesListingComponent } from './pages/demandes-listing/demandes-listing.component';
 import { DemandesEditComponent } from './pages/demandes-edit/demandes-edit.component';
+import { DemandesAddComponent } from './pages/demandes-add/demandes-add.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'demandes', component: DemandesListingComponent },
-  { path: 'demandes/add', component: DemandesEditComponent },
+  { path: 'demandes/add', component: DemandesAddComponent },
   { path: 'demandes/:id', component: DemandesEditComponent },
 ];
